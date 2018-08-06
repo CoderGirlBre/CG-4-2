28b273aa797361fcb6d16eda52d26a9057c434f2
CoderGirlBre <41300229+CoderGirlBre@users.noreply.github.com>
2018-08-06 15:00:09 -0500

    Add files via upload

--- Array/Program.cs
+++ Array/Program.cs
@@ -6,7 +6,20 @@ namespace Array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Creates array up to 6
+            int[] numbers = new int[6];
+
+            //Assigning array to these values.
+            numbers[0] = 1;
+            numbers[1] = 1;
+            numbers[2] = 2;
+            numbers[3] = 3;
+            numbers[4] = 5;
+            numbers[5] = 8;
+
+            //Telling console to print array with the assigned values.
+            foreach (int item in numbers) { Console.WriteLine(item); }
+            Console.ReadLine();
         }
     }
 }
